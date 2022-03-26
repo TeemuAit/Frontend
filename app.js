f9ea223d44e2db3e7cfe10ff5cf9dda38389dad0
TeemuAit <tee.aittola@gmail.com>
2022-03-26 19:25:40 +0200

    up

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ console.log('Hello World');
 
 const btn = document.createElement('button');
 btn.innerText = 'Ruokavalio';
+
+function changeColor(obj){
+    obj.style.color = "Peru";
+}
+
+document.getElementById("Contact").onclick = changeBackGroundColorToMediumOrchid;
+
+function changeBackGroundColorToMediumOrchid(){
+    document.querySelector('body').style.background = "MediumOrchid";
+}
